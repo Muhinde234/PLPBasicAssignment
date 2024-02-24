c94d74c155e49b143f1ebb8acb9b9bea92f4a395
Muhinde234 <igirimpuhwedosta@gmail.com>
2024-02-24 19:04:25 +0200

    assignment

---
+++ PycharmProjects/plp_learning/user_input.py
@@ -0,0 +1,8 @@
+name=input("please enter your name: ")
+age=input("please enter your age : ")
+location=input("enter where you live : ")
+a= int(age)
+
+print("hello " + name)
+print("you are " +  str(a))
+print("you live  in "+ location)
